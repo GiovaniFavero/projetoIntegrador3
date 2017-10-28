69bdd5c4b0dab4f348ed7cc47bea2ef7afae415a
faver <faver@DESKTOP-30IQP9N.lan>
2017-10-28 09:33:46 -0200

    Primeiro Commit.

---
+++ src/main/java/br/com/udesc/ProjetoIntegrador3/ProjetoIntegrador3Application.java
@@ -0,0 +1,12 @@
+package br.com.udesc.ProjetoIntegrador3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegrador3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegrador3Application.class, args);
+	}
+}
---
+++ src/main/java/br/com/udesc/ProjetoIntegrador3/ServletInitializer.java
@@ -0,0 +1,13 @@
+package br.com.udesc.ProjetoIntegrador3;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ProjetoIntegrador3Application.class);
+	}
+
+}
